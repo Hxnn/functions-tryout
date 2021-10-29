cd7cd4c07436543f0709bc193786ef56cb7974c6
Hinn <64025546+TheHinn@users.noreply.github.com>
2021-10-29 09:16:36 +0200

    FIXED HELLO WORLDD

--- hello/hello-world.py
+++ hello/hello-world.py
@@ -1,8 +1,8 @@
 def helloWorld(x=1):
-    x = x * ('Hello World!')
+    x = x * ['Hello World!']
     for y in x:
         print(y)
-
+    return
 
 helloWorld(5)
 # Put a number here to print it that many times.
